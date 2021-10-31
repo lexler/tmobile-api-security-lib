de4d9e9f48edab8d09e365b08aafb2e47c46477f
Carl Prothman <carl.prothman1@t-mobile.com>
2021-10-31 09:49:03 -0700

    Clean up solution - Remove .net 4.5.2 framework projects. - Remove .net 4.8 framework projects. - Keep .net core 3.1 projects

--- encryption-lib/CS-Encryption-Lib/Example_Asp.Net_Mvc_WebApplication_4.5.2/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace Example_Asp.Net_Mvc_WebApplication_4._5._2.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}

dfc0cbfae7dea8eae3878afa34244488cda7a48f
Kevin L. Mitchell <kevin.mitchell71@t-mobile.com>
2022-12-16 17:59:06 -0500

    Initial implementation of Go PoP token builder/validator
    This commit contains the initial implementation of the Go PoP token
    builder and validator package.  The package provides a PoPToken type, a
    New function to create it, and several options that may be provided to New
    to influence how the token is created.  The PoPToken type provides a
    Build method, to compute a PoP token from an HTTP request; a Sign method,
    to actually attach the computed PoP token to the HTTP request in the form
    of an "X-Authorization" header; and a Verify method, to verify that an
    HTTP request contains an "X-Authorization" header and to verify that it
    matches the rest of the request.
    
    Although the code is clean, according to the lint tests, this commit does
    not include any unit tests as of yet.  Additionally, the handling of
    repeated headers is not completely clear; the chosen implementation is
    based on RFC 9110's description of how repeated headers is to be handled,
    but it is not yet clear that this is the correct implementation of this
    case.  As such, the logic has been centralized into a single utility
    function, to allow easy refactoring should this not be the correct
    algorithm.

---
+++ poptoken-lib/poptoken-builder/go-lib-tmobile-oss-poptoken-builder/errors.go
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2022 Kevin L. Mitchell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package poptoken
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Errors that may be returned by the PoP token library.
+var (
+	ErrNoKeys       = errors.New("At least one of a public key or a private key must be provided")
+	ErrInvalidKeys  = errors.New("Public key does not belong to private key")
+	ErrNoPrivateKey = errors.New("No private key provided")
+	ErrNoPoPToken   = errors.New("No PoP token set in request")
+
+	// Invalid token errors; always wrapped in an InvalidToken
+	ErrParsedInvalid = errors.New("Token marked as invalid by JWT library")
+	ErrBadClaims     = errors.New("Token claims not interpretable")
+	ErrNoEHTSClaim   = errors.New("EHTS claim not available or not a string")
+	ErrNoEDTSClaim   = errors.New("EDTS claim not available or not a string")
+	ErrEDTSMismatch  = errors.New("Computed EDTS does not match claimed EDTS")
+)
+
+// InvalidToken is an error that optionally wraps another error and
+// which indicates that a token is not valid.
+type InvalidToken struct {
+	Err error // Wrapped error
+}
+
+// wrapInvalidToken is a helper that wraps an error with an
+// [InvalidToken].
+func wrapInvalidToken(e error) error {
+	return InvalidToken{
+		Err: e,
+	}
+}
+
+// Error implements the error interface and returns the error string.
+func (e InvalidToken) Error() string {
+	return "Pop token is not valid"
+}
+
+// FullError returns the full error, including the embedded error.
+// This is implemented as a non-standard error function to avoid
+// leaking too much data to the client which originated the invalid
+// token.
+func (e InvalidToken) FullError() string {
+	return fmt.Sprintf("PoP token is not valid: %s", e.Err)
+}
+
+// Unwrap implements the error unwrapping interface.
+func (e InvalidToken) Unwrap() error {
+	return e.Err
+}
+
+// FullError is a helper that reports the full error message of an
+// error originating from the [PoPToken.Verify] method.  This ensures
+// that the underlying cause of the verification error can be
+// retrieved without accidentally leaking any data back to the client.
+func FullError(e error) string {
+	var ivError InvalidToken
+
+	if errors.As(e, &ivError) {
+		return ivError.FullError()
+	}
+
+	return e.Error()
+}
---
+++ poptoken-lib/poptoken-builder/go-lib-tmobile-oss-poptoken-builder/go.mod
@@ -0,0 +1,8 @@
+module github.com/tmobile/tmobile-api-security-lib/poptoken-builder/go-lib-tmobile-oss-poptoken-builder
+
+go 1.18
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	gopkg.in/dgrijalva/jwt-go.v3 v3.2.0 // indirect
+)
